db6e3a293c5362b7a76a1f8dce9296c5b69b2ca5
jastiadirizal <kafinjastiadirizal@gmail.com>
2023-06-21 18:55:07 +0700

    memasukkan link netlify pada task.js

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL"; // TODO: replace this
+const url = "https://fin-photo-gallery.netlify.app/"; // TODO: replace this
 const studentName = "Muhammad Kafin Jastiadirizal Prasadana"; // TODO: replace this
 const studentId = "FE5654415"; // TODO: replace this
 
