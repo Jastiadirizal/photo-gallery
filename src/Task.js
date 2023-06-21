ad7d8db392ce67d858b7b2f6e9fe611bed9361af
jastiadirizal <kafinjastiadirizal@gmail.com>
2023-06-21 18:31:13 +0700

    Upload File Photo-Gallery

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Muhammad Kafin Jastiadirizal Prasadana"; // TODO: replace this
+const studentId = "FE5654415"; // TODO: replace this
+
+export { url, studentName, studentId };
